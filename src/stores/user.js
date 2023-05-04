1307a371c9856880a841e1a42b69e43df589f978
ainamanzanas <122228481+ainamanzanas@users.noreply.github.com>
2023-05-04 19:05:11 +0200

    Add files via upload

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,16 +1,39 @@
 import { defineStore} from "pinia";
-import supabase from '@supabase';
+import supabase from '@/supabase';
 
-export default defineStore('user', {
-    state: () => {
-        return {
-            user: null,
-        }
-    },
+export default defineStore('userStore', {
+    state: () => ({
+        user: undefined,
+    }),
+
     actions: {
         async fetchUser() {
             const { data: { user } } = await supabase.auth.getUser()
-            this.user = user
-        }
+
+            this.user = user;
+            if (!user) throw new Error('User not exsits');
+        },
+        async signUp({ email, password}) {
+            const { data, error } = await supabase.auth.signUp({
+                email,
+                password,
+            });
+            if (error) throw error;
+            if (data) this.user = data;
+        },
+        async signIn({ email, password }) {
+            const { data: { user }, error } = await supabase.auth.signInWithPassword({
+                email,
+                password,
+            });
+            if (error) throw error;
+            if (user) this.user = user;
+        },
+        async signOut() {
+            const { error } = await supabase.auth.signOut();
+            if (error) throw error;
+            this.user = null;
+        },
     }
-})
+});
+        
